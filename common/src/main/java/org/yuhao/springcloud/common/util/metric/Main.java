38f5a2c8c61259951549fcaa63412d18ea77923a
yuhao <hughieyu@163.com>
2020-06-30 18:38:33 +0800

    metric

---
+++ common/src/main/java/org/yuhao/springcloud/common/util/metric/Main.java
@@ -0,0 +1,16 @@
+package org.yuhao.springcloud.common.util.metric;
+
+/**
+ * 熔断器测试
+ */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        MetricStream metricStream = new MetricStream(10, 1000, 10, 50, 2);
+        for (int i = 0; i < 1000; i++) {
+            metricStream.addEvent(RequestEvent.FAIL);
+            Thread.sleep(500);
+            System.out.println(metricStream.allowRequest());
+        }
+    }
+}
